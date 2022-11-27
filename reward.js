1e19d86d0b70bbed2514b2663a2f5c1659a9b026
gonzalovazquez <gonzalovazquez010@gmail.com>
2022-11-27 00:43:44 -0500

    feat: 🎸 add tailwind table component

--- reward.js
+++ reward.js
@@ -1,10 +1,13 @@
 var t = TrelloPowerUp.iframe();
 
-window.reward.addEventListener('submit', function(event){
+var rewardBtn = document.querySelector('.add-reward');
+
+rewardBtn.addEventListener('onclick', function(event){
   // Stop the browser trying to submit the form itself.
   event.preventDefault();
   return t.set('card', 'shared', 'estimate', window.estimateSize.value)
   .then(function(){
+    console.log('Submit card')
     t.closePopup();
   });
 });
