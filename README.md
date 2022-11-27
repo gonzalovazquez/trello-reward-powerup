71963febb81bf06d01329a33d06fc2aa4b6b9dbb
gonzalovazquez <gonzalovazquez010@gmail.com>
2022-11-27 10:21:46 -0500

    fix: 🐛 remove second callback for card button

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
+[![Netlify Status](https://api.netlify.com/api/v1/badges/0e1b7089-0d82-4412-b9ce-839d59195274/deploy-status)](https://app.netlify.com/sites/wondrous-centaur-831e70/deploys)
+
 # trello-reward-powerup
+
+A Trello PowerUp that links your Reward Gallery to Card.
--- client.js
+++ client.js
@@ -4,13 +4,9 @@ TrelloPowerUp.initialize({
       icon: 'https://cdn-icons-png.flaticon.com/256/6735/6735255.png',
       text: 'Claim Reward!',
       callback: function(t) {
-        t.card('id', 'name').then(card => {
-          console.log(`Id: ${card.id}`);
-          console.log(`name: ${card.name}`)
-          return t.popup({
-            title: "Reward",
-            url: 'reward.html',
-          })
+        return t.popup({
+          title: "Reward",
+          url: 'reward.html',
         })
       }
     }];
